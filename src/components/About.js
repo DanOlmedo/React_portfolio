d8c2662a1fdea58bc2ce5168c22d4186febb8de2
Daniel Olmedo <olmedo.daniel96@gmail.com>
2022-07-20 20:02:43 -0500

    Added some react components

---
+++ src/components/About.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function About() {
+
+    return (
+        <>
+            <div class="about">
+    
+            <div class="row">
+                    <svg class="tri1" >
+                        <polygon id="tri1" points="189.38,216.54 128.76,321.54 250,321.54"/>
+                        <polygon id="tri2" points="310.62,216.54 250,321.54 371.24,321.54"/>
+                        <polygon id="tri3" points="250,111.54 189.38,216.54 310.62,216.54"/>
+                        <polygon id="tri4" points="189.38,216.54 250,321.54 310.62,216.54"/>
+                    </svg>
+            <div/>
+            <div class="row">            
+                       <p> Full-stack web developer with an aptitude for problem solving applications and animation. Back-end experience with Python (Django) and JavaScript, and bootstrap for front-end.
+                        I've completed a couple of deployed applications (linked further down), and currently strenghtening my knowledge with a full-stack web development bootcamp.
+                        </p>
+                </div>
+                </div>
+            </div>
+        </>
+    )
+ };
+
+ export default About;
---
+++ src/components/Info.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function Info() {
+
+    return (
+        <>
+        <h1>Info</h1>
+        <h2>Cards aint working</h2>
+        <div class="cards">
+            <div class="card">
+            <img class="card-img-top" src="..." alt="Card image cap"></img>
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+            <div class="card">
+            <img class="card-img-top" src="..." alt="Card image cap"></img>
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+            <div class="card">
+            <img class="card-img-top" src="..." alt="Card image cap"></img>
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+            </div>
+        </>
+    )
+};
+
+export default Info;
