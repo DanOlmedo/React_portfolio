f106d9796480febf0b212e9c7918cf8a92cb4198
Daniel Olmedo <olmedo.daniel96@gmail.com>
2022-07-22 18:10:31 -0500

    Added images for carousel

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Cash from '../img/Cash_boomerang.PNG';
+import Nifty from '../img/Nifty_1.PNG';
+import Scheduler from '../img/day_scheduler_1.PNG';
 
 function Projects () {
     return (
@@ -8,13 +11,13 @@ function Projects () {
         <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="..." alt="First slide" />
+      <img class="carImage" src={Cash} alt="Cash boomerang app" />
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Second slide" />
+      <img class="carImage" src={Nifty} alt="Nifty outfit creator app" />
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Third slide" />
+      <img class="carImage" src={Scheduler} alt="Day scheduler app" />
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
